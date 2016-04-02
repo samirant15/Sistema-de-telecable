90d2ef744cddd8bfaacef39c6386c7963bdfed90
juandiii <juandiegolopezve@gmail.com>
2016-04-02 01:11:31 -0300

    Agrege sistema de logueo
    Changelog: Agrege sistema de logueo con contraseña!.

--- main.cpp
+++ main.cpp
@@ -2,26 +2,90 @@
 
 using namespace std;
 
+string loginadmin;
+string loginadminpass;
+int session_logged = 0;
+
+//Administradores
+int adminsLength = 3;
+string admins[20] = {"JuanDi", "Cristian", "Samir"};
+string adminspass[20] = {"lolo123","cristian123", "samir123"};
+    
+//Planes
+int planesLength = 4;
+string Planes[10] = {"Sencillo", "Normal", "Bueno", "Premium"};
+int precioBase[10] = {200, 300, 500, 800};
+int precioPorCaja[10] = {100, 150, 200, 300};
+int paqueteDeCanales[10] = {5, 10, 15, 20};
+
+//Clientes
+int clientesLength = 5;
+string clientes[50] = {"Pedro", "Jose", "Estephany", "Saul", "Anabel"};
+int cedula[50] = {13520314789, 51234756932, 13604893265, 635586901275}; //las cedulas tienen 11 numeros
+string planRegistrado[50] = {"Normal", "Bueno", "Premium", "Normal"};
+int cantCajas[50] = {2, 1, 3, 5};
+int mensualidad[50] = {};//Hay que calcularla con el plan y la cantidad de cajas
+
+//Funcion Buscar la posicion del administrador.
+int BuscarAdministrador(string loginadmin){
+	for(int i=0; i<3; i++){
+	    if(loginadmin == admins[i]){
+	        return i;
+	    }
+	}
+}
+
+//Funcion Logueo
+void logeo(){
+	string loginadmin;
+	cout << "> Nombre: ";
+    cin >> loginadmin;
+    cout << "> Contraseña: ";
+    cin >> loginadminpass;
+	int pos = BuscarAdministrador(loginadmin);
+	
+	if(loginadmin == admins[pos] && loginadminpass == adminspass[pos]){
+			session_logged = 1;
+	} else {
+		cout << "Su contraseña o nombre de usuario es incorrecto!";
+	}
+}
+
+void logged(){
+	if(session_logged == 1){
+		cout << "a. Agregar clientes." << endl;
+		cout << "b. Borrar Clientes." << endl;
+		cout << "c. Agregar mas cajitas" << endl;
+		cout << "d. Salir del Administrador" << endl;
+		
+		char opcion;
+		cin >> opcion;
+		switch(opcion){
+			case 'a':
+				//Colocar Funcion de Agregar Clientes
+				break;
+			case 'b':
+				//Colocar Funcion de Borrar Clientes
+				break;
+			case 'c':
+				//Colocar Funcion de Agregar Mas Cajitas
+				break;
+			case 'd':
+				//Salir del Administrador
+				session_logged = 0;
+				logeo();
+				break;
+			default: 
+				cout << "Error 404 -> Selecciona la opcion correctamente." << endl;
+				logged();
+				break;		
+		}
+	}
+}
+
+
 int main()
 {
-    //Administradores
-    int adminsLength = 3;
-    string admins[20] = {"JuanDi", "Cristian", "Samir"};
-    string adminspass[20] = {"lolo123","cristian123", "samir123"};
-
-    //Planes
-    int planesLength = 4;
-    string Planes[10] = {"Sencillo", "Normal", "Bueno", "Premium"};
-    int precioBase[10] = {200, 300, 500, 800};
-    int precioPorCaja[10] = {100, 150, 200, 300};
-    int paqueteDeCanales[10] = {5, 10, 15, 20};
-
-    //Clientes
-    int clientesLength = 5;
-    string clientes[50] = {"Pedro", "Jose", "Estephany", "Saul", "Anabel"};
-    int cedula[50] = {13520314789, 51234756932, 13604893265, 635586901275}; //las cedulas tienen 11 numeros
-    string planRegistrado[50] = {"Normal", "Bueno", "Premium", "Normal"};
-    int cantCajas[50] = {2, 1, 3, 5};
-    int mensualidad[50] = {};//Hay que calcularla con el plan y la cantidad de cajas
-
+	logeo();
+	logged();
 }
